393def6c42496394e91c80058389b9660a20786c
Roland Lehner <roland.lehner@teilchen.net>
2018-05-31 11:41:00 +0200

    Implement Remote-Control Interface & SideMenu
    Implemented first impression of the remote control of the radio as well as including in the sidemenu with changing the default page

--- src/pages/control/control.ts
+++ src/pages/control/control.ts
@@ -15,6 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ControlPage {
 
+  private time = 0;
+  private newtime = "0:00:00";
+  
+  onChange(ev: any) {
+    //console.log('Changed', ev);
+    //console.log(this.time);
+    let hours;
+    let minutes;
+    let seconds;
+    //console.log(this.time);
+    
+    seconds = this.time % 60
+    minutes = Math.floor(this.time / 60);
+    hours = Math.floor(minutes / 60);
+      
+    //Führende 0 wenn Sekunden < 10
+    if (seconds < 10) {
+      seconds = '0' + seconds;
+    }
+    //Bei jeder vollen Stunde bei 0 min beginnen
+    if (hours >= 1) {
+      minutes = minutes - (60 * hours);
+    }
+    //Führende Nullen für die Minuten
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }      
+    //console.log(hours + ':' + minutes + ':' + seconds);
+    this.newtime = hours + ':' + minutes + ':' + seconds;
+  }
+    
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
