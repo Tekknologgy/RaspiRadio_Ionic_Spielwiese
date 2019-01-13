c09e071d1c44ac25a1d8971744419f0eb809f6e8
Tekknologgy <32256226+Tekknologgy@users.noreply.github.com>
2019-01-13 18:27:16 +0100

    Login-Page & Connecting
    Added Login-Page and functionality for Connecting and displaying Login-Page;
    
    Missing:
    Remove duration in loadingctrl and add wait() timer for simulating connecting

--- RaspiRadio/src/app/home/home.page.ts
+++ RaspiRadio/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, NavController, LoadingController} from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public alertCtrl: AlertController, public loadingCtrl: LoadingController, public navCtrl : NavController) {
 
+  }
+  port;
+  ip;
+
+
+  async conn_rasp() {
+    if((this.ip & this.port) > 0){
+      this.show_loading();
+    }
+    else this.err_report();
+  }
+
+  async err_report() {
+    const alert = await this.alertCtrl.create({
+      header: 'Verbindung Fehlgeschlagen',
+      buttons: [{
+        text: 'Daten erneut eingeben',
+        handler: () => {
+          alert.dismiss(false);
+        }
+      }]
+    });
+    return await alert.present();
+  }
+
+  async show_loading() {
+    const loading = await this.loadingCtrl.create({
+      message:'Connecting...',
+      duration:2000
+    });
+    /*
+    const alert = await this.alertCtrl.create({
+      header:'Verbindung Erfolgreich',
+      buttons: [{
+        text: 'Ok',
+        handler: () => {
+          alert.dismiss(false);
+          this.navCtrl.navigateForward('/login');
+        }
+      }]
+    });
+    */
+    //return await loading.present(),await alert.present();
+    
+    //await loading.onDidDismiss();
+    //this.navCtrl.navigateForward('/login');
+    
+    return await loading.present();
+  }
 }
