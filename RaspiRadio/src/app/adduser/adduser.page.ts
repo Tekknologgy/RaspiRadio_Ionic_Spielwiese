399ff6c028057335ffcecf486b0770bc82fbfaf9
sebi <hautz.s90@htl-ottakring.ac.at>
2019-02-16 17:27:23 +0100

    player update
    Player: Tab leiste angepasst. anfangs laufzeit dynamisch gemacht.
    AddUser: Bei den Farbbuttons wird jetzt die id richtig ausgelesen. und in der console ausgegeben.

--- RaspiRadio/src/app/adduser/adduser.page.ts
+++ RaspiRadio/src/app/adduser/adduser.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adduser.page.scss'],
 })
 export class AdduserPage implements OnInit {
-
+  public value;
   constructor() { }
-
+  
+  
+ async getColor($event) { 
+  console.log($event.target.id);
+  await console.log($event.target.id);
+  await console.log(this.value);
+   console.log("test");
+  }
   ngOnInit() {
   }
-
 }
