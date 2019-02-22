6179caec9f1ebc76556e1480a1265d021f25a539
sebi <hautz.s90@htl-ottakring.ac.at>
2019-02-22 17:38:48 +0100

    user array hinzugefügt
    user array wrude hinzugefügt der befüllt wird auf knopfdruck und abgespeichert. und für testzwecke ausgegebne

--- RaspiRadio/src/app/adduser/adduser.page.ts
+++ RaspiRadio/src/app/adduser/adduser.page.ts
@@ -8,15 +8,34 @@ import {NavController} from '@ionic/angular';
 })
 export class AdduserPage implements OnInit {
   username:string;
+  bg;
+  user=[
+    {id:''},
+    {username:''},
+    {bgcolor:''},
+    {playlistid:''}
+  ]
   constructor(public navCtrl : NavController) {}
   
  async getColor($event) { 
+  this.bg = $event.target.id;
+  await console.log(this.bg);
   await console.log($event.target.id);
+  
 }
 
   async saveuser(){
-    await console.log(this.username);
-    this.navCtrl.navigateForward('/login');
+    //await console.log(this.username);
+    //await console.log(this.bg);
+    //this.navCtrl.navigateForward('/login');
+    this.user.push(
+      {id:'1'},
+      {username:this.username},
+      {bgcolor:this.bg}
+    )
+    for(let e of this.user){
+      console.log(e);
+    }
   }
 
   ngOnInit() {
