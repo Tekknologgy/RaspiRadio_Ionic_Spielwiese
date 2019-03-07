a5e135b22191e61d8932343900854968cf1dafe5
Markus <nessel.m87@htl-ottakring.ac.at>
2019-03-07 10:13:38 +0100

    Update raspiradio_server.py
    Next and Previous added

--- raspiradio_server.py
+++ raspiradio_server.py
@@ -3,6 +3,7 @@
 #
 
 from mpd import MPDClient
+from pathlib import Path
 import json
 import asyncio
 import websockets
@@ -62,9 +63,86 @@ async def hello(websocket, path):
                     client.connect("localhost", 6600)
                     client.stop()
                     #print("Exception")
+            ###Next
+            if parsed_json['Action'] == "Next":
+                print("Next")
+                try:
+                    #if client.playlistid(status['nextsongid']) != client.playlistid('NULL'):
+                        print("try")
+                        status = {}
+                        status = client.status()
+                        print("status")
+                        songinfo_list = client.playlistid(status['nextsongid']) #???????
+                        print(songinfo_list)
+                        #songinfo_string = ''.join(str(e) for e in songinfo_list) #???????
+                        #print(songinfo_string)
+                        #songinfo = ast.literal_eval(songinfo_string) #???????
+                        #print(songinfo)
+                        command = {"Action": "State", "State": "Playing", "Title": "Servertest Next"}
+                        #command = {"Action": "NEXT", "State": "Play", "Titel": songinfo['title']} #???????
+                        dumped_json = json.dumps(command)
+                        await websocket.send(dumped_json)
+                        client.next()
+                        print("JSON Next")
+                    #else:
+                        #client.playlistid(status['nextsongid']) == client.playlistid(status['previoussongid'])
+                        #print("Error abgefangen")
+                except:
+                    client.connect("localhost", 6600)
+                    client.stop()
+                    print("Exception")
+            ###Previous
+            if parsed_json['Action'] == "Previous":
+                print("Previous")
+                try:
+                    print("try")
+                    status = {}
+                    status = client.status()
+                    print("status")
+                    #songinfo_list = client.playlistid(status['nextsongid']) #???????
+                    print("songinfo_list")
+                    #songinfo_string = ''.join(str(e) for e in songinfo_list) #???????
+                    #print(songinfo_string)
+                    #songinfo = ast.literal_eval(songinfo_string) #??????
+                    #print(songinfo)
+                    command = {"Action": "State", "State": "Playing", "Title": "Servertest Previous"}
+                    #command = {"Action": "Previous", "State": "Play", "Titel": songinfo['title']}
+                    dumped_json = json.dumps(command)
+                    await websocket.send(dumped_json)
+                    client.previous()
+                    print("JSON Previous")
+                except:
+                    client.connect("localhost", 6600)
+                    client.stop()
+                    print("Exception")
+            ####AddUsers
+            if parsed_json['Action'] == "AddUser":
+                #print("JSON Adduser before Try")
+                try:
+                    data = (parsed_json['Name']) + "," + (parsed_json['Color'])
+                    #print("JSON Adduser nach json_parse")
+                    path = '/home/pi/Python-Scripts/Userdatabase.txt'
+                    useres_file = open(path, 'r+')
+                    #print("JSON Adduser nach erstellung der  txt")
+                    useres_file.write(data)
+                    useres_file.close()
+                    #print("JSON Adduser")
+                except:
+                    client.connect("localhost", 6600)
+                    client.stop()
+                    print("Exception")
             ### GetState
             if parsed_json['Action'] == "getState":
-                await websocket.send('{"Action": "State","Title": "Muuh","Artist": "Kuh","Duration": 70,"Elapsed": 55,"Volume": 60,"State": "Playing"}')
+                #print("JSON GetState")
+                try:
+                    command = {"Action": "State","Title": "Server","Artist": "Send getState","Duration": 666,"Elapsed": 42,"Volume": 60,"State": "Playing"}
+                    dumped_json = json.dumps(command)
+                    await websocket.send(dumped_json)
+                    #print("JSON GetState send")
+                except:
+                    client.connect("localhost", 6600)
+                    client.stop()
+                    print("Exception")
 
         except:
             #print("Exception")
